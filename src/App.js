dc5245dbaa1239b57777e3e7910fdd01b01b9e35
Ruslan Kuzma <kuzrus31@gmail.com>
2021-10-03 20:26:37 +0300

    добавил телефонную книгу

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList'
+import Filter from './components/Filter/Filter';
+import Container from './components/Container/Container.js'
+
+
+
+class App extends Component{
+
+  state = {
+contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+  
+
+  }
+  addContact = (task) => {
+
+    const searchSameName = this.state.contacts.map((cont) => cont.name).includes(task.name);
+    
+    if (searchSameName) {
+      alert(`${task.name} is already in contacts` )
+    }
+    else if (task.name.length === 0) {
+      
+      alert("Fields must be filled!");
+    }
+      
+      else {
+      
+      const contact = { ...task, id: uuidv4(), }
+       this.setState((prevState) => ({contacts: [...prevState.contacts, contact]}))
+    }
+  };
+ 
+
+  getVisiebileContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter((contacts) => contacts.name.toLowerCase().includes(filter.toLowerCase()))
+
+
+  }
+  
+
+  deleteContact = contactId => {
+    this.setState((prevState) => {
+     return{contacts: prevState.contacts.filter(({id}) => id !== contactId),} 
+    });
+   
+  };
+  changeFilter = (e) => {
+
+    this.setState({ filter: e.currentTarget.value });
+  }
+
+  render(){
+    const { filter } = this.state;
+     const visibleContacts = this.getVisiebileContacts();
+    return (
+      
+      <Container>
+        
+        <ContactForm onAddContact={this.addContact} />
+
+        {
+          visibleContacts.length > 1 && (<Filter  value={filter} onChangeFilter={this.changeFilter}/>)
+        }
+        {visibleContacts.length > 0 && ( <ContactList contacts={visibleContacts} onDeleteContact={this.deleteContact} />)}
+       
+
+        
+        </Container>
+    )
+  }
+
+}
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+
+import s from './container.module.css';
+
+function Container({ children }) {
+  return <div className={s.container}>{children}</div>;
+}
+
+
+
+
+
+export default Container
